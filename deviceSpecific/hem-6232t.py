5020c5562aaac30714411e19bf6f19eb5d75fd75
Benjamin <12860358+userx14@users.noreply.github.com>
2023-05-13 01:40:04 +0200

    Update hem-6232t.py

--- deviceSpecific/hem-6232t.py
+++ deviceSpecific/hem-6232t.py
@@ -21,12 +21,12 @@ class deviceSpecificDriver(sharedDeviceDriverCode):
 
     def deviceSpecific_ParseRecordFormat(self, singleRecordAsByteArray):
         recordDict             = dict()
-        recordDict["dia"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 0, 6)
+        recordDict["dia"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 0, 7)
         recordDict["sys"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 8, 15) + 25
-        year                   = self._bytearrayBitsToInt(singleRecordAsByteArray, 16, 23) + 2000
+        year                   = self._bytearrayBitsToInt(singleRecordAsByteArray, 16, 21) + 2000
         recordDict["bpm"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 24, 31)
-        recordDict["mov"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 32, 32)
-        recordDict["ihb"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 33, 33)
+        recordDict["ihb"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 32, 32)
+        recordDict["mov"]      = self._bytearrayBitsToInt(singleRecordAsByteArray, 33, 33)
         month                  = self._bytearrayBitsToInt(singleRecordAsByteArray, 34, 37)
         day                    = self._bytearrayBitsToInt(singleRecordAsByteArray, 38, 42)
         hour                   = self._bytearrayBitsToInt(singleRecordAsByteArray, 43, 47)
