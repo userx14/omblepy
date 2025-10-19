51394f4d813facae798d6c1f868469eb993fe62e
Benjamin <12860358+userx14@users.noreply.github.com>
2025-10-19 19:52:14 +0200

    fix win11 timeout issue
    Update README.md

--- omblepy.py
+++ omblepy.py
@@ -360,7 +360,7 @@ async def main():
     try:
         logger.info(f"Attempt connecting to {bleAddr}.")
         await bleClient.connect()
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(10)   #time delay for user to accept os pairing dialog on win11
         await bleClient.pair(protection_level = 2)
         #verify that the device is an omron device by checking presence of certain bluetooth services
         if parentService_UUID not in [service.uuid for service in bleClient.services]:
